8f815d573f8fd16e6b02bd5fbf14d73e821b9218
kiruthika20 <76843167+kiruthika20@users.noreply.github.com>
2021-06-18 11:39:33 +0530

    Create employee.sql

---
+++ employee.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM database1.employee
+LIMIT 0, 1000
+
+-- Date: 2021-06-18 08:54
+*/
+INSERT INTO `` (`employee id`,`first name`,`last name`,`age`,`department name`) VALUES (125,'manlini','govindh',22,'software trainee');
+INSERT INTO `` (`employee id`,`first name`,`last name`,`age`,`department name`) VALUES (167,'madhu','mitha',26,'associate software engineer');
+INSERT INTO `` (`employee id`,`first name`,`last name`,`age`,`department name`) VALUES (412,'jeevan',NULL,26,'testing');
+INSERT INTO `` (`employee id`,`first name`,`last name`,`age`,`department name`) VALUES (456,'catherine',NULL,25,'ios developer');
+INSERT INTO `` (`employee id`,`first name`,`last name`,`age`,`department name`) VALUES (567,'sandhiya',NULL,26,'testing');
+INSERT INTO `` (`employee id`,`first name`,`last name`,`age`,`department name`) VALUES (987,'dibesh','babu',29,'development and maintanence');
